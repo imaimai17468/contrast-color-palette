c692faba97c53abf835e011aebf123989e3effec
imaimai17468 <tosiki_801_life@outlook.jp>
2024-12-19 11:09:10 +0900

    fix: adjust padding in PalletContent and Header components for improved responsiveness

--- src/components/parts/header.tsx
+++ src/components/parts/header.tsx
@@ -1,6 +1,6 @@
 export const Header = (): React.ReactNode => {
   return (
-    <header className="sticky top-0 z-50 py-4 px-8 flex gap-4 items-center justify-between bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10">
+    <header className="sticky top-0 z-50 py-2 sm:py-4 px-4 sm:px-8 flex gap-4 items-center justify-between bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10">
       <div className="flex flex-col gap-2">
         <p className="text-2xl font-bold">Contrast Color Picker</p>
         <p className="text-sm text-muted-foreground">
