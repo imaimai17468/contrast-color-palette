440cf21551d05c60b139f64a40ec3775da5231d8
imaimai17468 <tosiki_801_life@outlook.jp>
2025-02-07 02:16:58 +0900

    refactor: update header title from "Contrast Color Picker" to "Contrast Color Palette"

--- src/components/parts/header.tsx
+++ src/components/parts/header.tsx
@@ -2,7 +2,7 @@ export const Header = (): React.ReactNode => {
   return (
     <header className="sticky top-0 z-50 py-2 sm:py-4 px-4 sm:px-8 flex gap-4 items-center justify-between bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10">
       <div className="flex flex-col gap-2">
-        <p className="text-2xl font-bold">Contrast Color Picker</p>
+        <p className="text-2xl font-bold">Contrast Color Palette</p>
         <p className="text-sm text-muted-foreground">
           <em>oklch()</em> + lightness curve / color palette for dark theme as well as light theme
         </p>
